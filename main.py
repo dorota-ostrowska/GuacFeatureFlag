cebd57a0286f455bc33917a244876fdac0aff9a4
dorota <dorotaostrowska.666@gmail.com>
2025-01-13 18:01:52 +0100

    init

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, Blueprint, render_template
+
+recipe = Blueprint("recipe", __name__, template_folder="templates")
+
+@recipe.route("/")
+def display_recipe():
+    return render_template("recipe.html")
+
+app = Flask(__name__)
+app.register_blueprint(recipe, url_prefix="/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
